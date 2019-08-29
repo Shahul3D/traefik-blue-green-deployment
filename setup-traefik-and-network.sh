02d2bb3e0e0c23b6f479cd99060dd835fcc0ad38
Shahul Hameed S <3414602+Shahul3D@users.noreply.github.com>
2019-08-30 00:08:10 +0530

    fixed a mistake..
    network name should be set to env on both the cases.

--- setup-traefik-and-network.sh
+++ setup-traefik-and-network.sh
@@ -5,8 +5,8 @@ NETWORK_NAME="$1"
 if [ -z "$NETWORK_NAME" ]
 then
     NETWORK_NAME="traefik_network"
-    export NETWORK_NAME="$NETWORK_NAME"
 fi
+    export NETWORK_NAME="$NETWORK_NAME"
 
 if [ -z $(docker network ls --filter "name=$NETWORK_NAME" --format "{{.Name}}") ]
 then
